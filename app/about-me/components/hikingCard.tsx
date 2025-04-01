33e91aaae4dce109ec43c6bbae9f5f0c8b0bc4a5
michael-kabat <michaelckabat@gmail.com>
2025-04-01 18:54:19 -0400

    testing img vs Image

--- app/about-me/components/hikingCard.tsx
+++ app/about-me/components/hikingCard.tsx
@@ -1,28 +1,33 @@
 import React from "react";
-
+import Image from "next/image";
 const HikingCard = () => {
     return (
         <div className="m-10 card card-side bg-base-200 shadow-sm ">
             <figure>
-                <img
+                {/* <img
+                    src="images\standingOnLogPicture.jpg"
+                    className="object-cover h-120 w-115 rounded-lg ml-4 mt-2 mr-2"
+                    alt="Standing on a log over a lake"
+                /> */}
+                <Image
                     src="images\standingOnLogPicture.jpg"
                     className="object-cover h-120 w-115 rounded-lg ml-4 mt-2 mr-2"
                     alt="Standing on a log over a lake"
                 />
             </figure>
             <div className="card-body">
-                <img
+                <Image
                     src="images\lakeSerenePicture.jpg"
                     className="flex-row-reverse object-cover h-59 w-100 rounded-lg shadow-lg mt-4"
                     alt="Lake Serene hike"
                 />
-                <img
+                <Image
                     src="images\oregonHikePicture.jpg"
                     className="flex-row-reverse object-cover h-59 w-100 rounded-lg shadow-lg mb-2"
                     alt="Oregon hike"
                 />
             </div>
-            <img
+            <Image
                 src="images\cypressLandscapePicture.jpg"
                 className="flex-row-reverse object-cover h-120 w-150 rounded-lg shadow-lg m-2 mr-4 mt-10"
                 alt="Cypress Mountain hike"
