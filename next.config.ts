a3cab7c5bd197fd1fbdd672e3014f4b051ecef7f
michael-kabat <michaelckabat@gmail.com>
2025-04-01 18:11:46 -0400

    github pages?

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio",
+  output: "export"  // <=== enables static exports
+
 };
 
 export default nextConfig;
