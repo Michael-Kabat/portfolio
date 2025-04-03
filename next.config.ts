7968759caae25230a69b8c3b2e0c60a7b287cb01
michael-kabat <michaelckabat@gmail.com>
2025-04-02 23:07:34 -0400

    probs wont work with pages, but does work with npm run dev, check readme for pages instructions

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here 
   ### COMMENT THESE OUT IF RUNNING 'NPM RUN DEV' ###
   */
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio",
-  output: "export"  // <=== enables static exports
+  // basePath: "/portfolio",
+  // assetPrefix: "/portfolio",
+  // output: "export"  // <=== enables static exports
 
 };
 
